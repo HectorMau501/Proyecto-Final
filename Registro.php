f996e5153d5e30cadf00cbefa1ad92c940908ca8
HectorMau501 <mauhector7@gmail.com>
2023-04-20 16:01:18 -0600

    20/04/2023

---
+++ Registro.php
@@ -0,0 +1,24 @@
+<?php
+
+include "Conexion.php";
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$contrasena = $_POST['password'];
+$direccion = $_POST['direccion'];
+$telefono = $_POST['telefono'];
+
+$sql = mysqli_query($con, "INSERT INTO usuario (id,nombre,correo,password,telefono,direccion) 
+values (0,'$nombre', '$correo', '$contrasena', '$telefono', '$direccion')");
+
+if($sql){
+    header("location:Home.html");
+}
+else{
+    echo " Usuario no agregado";
+}
+
+
+
+?>
+
